dba3eeae5c97d3484bea33ddccddc7b37464b132
henrytorres9 <hentores9@gmail.com>
2023-10-18 21:39:35 -0600

    Funcionalidad de entrenamiento que detecta objetos

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template, request, jsonify
 import os
+import cv2
+import numpy as np
+
+
+# Cargar la configuración y los pesos pre-entrenados
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+layer_names = net.getUnconnectedOutLayersNames()
+
+# Cargar las clases
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
 
 app = Flask(__name__)
 
