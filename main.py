63ac908d8aae55f4c35d575ad19c833811e5a91c
henrytorres9 <66438500+henrytorres9@users.noreply.github.com>
2023-10-18 22:39:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import numpy as np
 app = Flask(__name__)
 
 
-# Cargar la configuración y los pesos pre-entrenados
-net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
-layer_names = net.getUnconnectedOutLayersNames()
+# # Cargar la configuración y los pesos pre-entrenados
+# net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+# layer_names = net.getUnconnectedOutLayersNames()
 
-# Cargar las clases
-with open("coco.names", "r") as f:
-    classes = [line.strip() for line in f.readlines()]
+# # Cargar las clases
+# with open("coco.names", "r") as f:
+#     classes = [line.strip() for line in f.readlines()]
 
 
 @app.route('/')
